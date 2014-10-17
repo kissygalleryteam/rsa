9d8521813a31b31de5895735da5912c31ec4b4d6
弘树 <tiehang.lth@alibaba-inc.com>
2014-10-17 10:43:23 +0800

    sync doc

--- doc/rng.js
+++ doc/rng.js
@@ -3,88 +3,86 @@
  * @author 梧忌 <wuji.xwt@alibaba-inc.com>
  * @module rng
  **/
-KISSY.add(function (S, prng_newstate) {
-  // Pool size must be a multiple of 4 and greater than 32.
-  // An array of bytes the size of the pool will be passed to init()
-  var rng_psize = 256;
+var prng_newstate = require('./prng4');
 
-  // Random number generator - requires a PRNG backend, e.g. prng4.js
+// Pool size must be a multiple of 4 and greater than 32.
+// An array of bytes the size of the pool will be passed to init()
+var rng_psize = 256;
 
-  // For best results, put code like
-  // <body onClick='rng_seed_time();' onKeyPress='rng_seed_time();'>
-  // in your main HTML document.
+// Random number generator - requires a PRNG backend, e.g. prng4.js
 
-  var rng_state;
-  var rng_pool;
-  var rng_pptr;
+// For best results, put code like
+// <body onClick='rng_seed_time();' onKeyPress='rng_seed_time();'>
+// in your main HTML document.
 
-  // Mix in a 32-bit integer into the pool
-  function rng_seed_int(x) {
-    rng_pool[rng_pptr++] ^= x & 255;
-    rng_pool[rng_pptr++] ^= (x >> 8) & 255;
-    rng_pool[rng_pptr++] ^= (x >> 16) & 255;
-    rng_pool[rng_pptr++] ^= (x >> 24) & 255;
-    if(rng_pptr >= rng_psize) rng_pptr -= rng_psize;
+var rng_state;
+var rng_pool;
+var rng_pptr;
+
+// Mix in a 32-bit integer into the pool
+function rng_seed_int(x) {
+  rng_pool[rng_pptr++] ^= x & 255;
+  rng_pool[rng_pptr++] ^= (x >> 8) & 255;
+  rng_pool[rng_pptr++] ^= (x >> 16) & 255;
+  rng_pool[rng_pptr++] ^= (x >> 24) & 255;
+  if(rng_pptr >= rng_psize) rng_pptr -= rng_psize;
+}
+
+// Mix in the current time (w/milliseconds) into the pool
+function rng_seed_time() {
+  rng_seed_int(new Date().getTime());
+}
+
+// Initialize the pool with junk if needed.
+if(rng_pool == null) {
+  rng_pool = new Array();
+  rng_pptr = 0;
+  var t;
+  if(window.crypto && window.crypto.getRandomValues) {
+    // Use webcrypto if available
+    var ua = new Uint8Array(32);
+    window.crypto.getRandomValues(ua);
+    for(t = 0; t < 32; ++t)
+      rng_pool[rng_pptr++] = ua[t];
   }
-
-  // Mix in the current time (w/milliseconds) into the pool
-  function rng_seed_time() {
-    rng_seed_int(new Date().getTime());
+  if(navigator.appName == "Netscape" && navigator.appVersion < "5" && window.crypto) {
+    // Extract entropy (256 bits) from NS4 RNG if available
+    var z = window.crypto.random(32);
+    for(t = 0; t < z.length; ++t)
+      rng_pool[rng_pptr++] = z.charCodeAt(t) & 255;
+  }  
+  while(rng_pptr < rng_psize) {  // extract some randomness from Math.random()
+    t = Math.floor(65536 * Math.random());
+    rng_pool[rng_pptr++] = t >>> 8;
+    rng_pool[rng_pptr++] = t & 255;
   }
+  rng_pptr = 0;
+  rng_seed_time();
+  //rng_seed_int(window.screenX);
+  //rng_seed_int(window.screenY);
+}
 
-  // Initialize the pool with junk if needed.
-  if(rng_pool == null) {
-    rng_pool = new Array();
-    rng_pptr = 0;
-    var t;
-    if(window.crypto && window.crypto.getRandomValues) {
-      // Use webcrypto if available
-      var ua = new Uint8Array(32);
-      window.crypto.getRandomValues(ua);
-      for(t = 0; t < 32; ++t)
-        rng_pool[rng_pptr++] = ua[t];
-    }
-    if(navigator.appName == "Netscape" && navigator.appVersion < "5" && window.crypto) {
-      // Extract entropy (256 bits) from NS4 RNG if available
-      var z = window.crypto.random(32);
-      for(t = 0; t < z.length; ++t)
-        rng_pool[rng_pptr++] = z.charCodeAt(t) & 255;
-    }  
-    while(rng_pptr < rng_psize) {  // extract some randomness from Math.random()
-      t = Math.floor(65536 * Math.random());
-      rng_pool[rng_pptr++] = t >>> 8;
-      rng_pool[rng_pptr++] = t & 255;
-    }
-    rng_pptr = 0;
+function rng_get_byte() {
+  if(rng_state == null) {
     rng_seed_time();
-    //rng_seed_int(window.screenX);
-    //rng_seed_int(window.screenY);
+    rng_state = prng_newstate();
+    rng_state.init(rng_pool);
+    for(rng_pptr = 0; rng_pptr < rng_pool.length; ++rng_pptr)
+      rng_pool[rng_pptr] = 0;
+    rng_pptr = 0;
+    //rng_pool = null;
   }
+  // TODO: allow reseeding after first request
+  return rng_state.next();
+}
 
-  function rng_get_byte() {
-    if(rng_state == null) {
-      rng_seed_time();
-      rng_state = prng_newstate();
-      rng_state.init(rng_pool);
-      for(rng_pptr = 0; rng_pptr < rng_pool.length; ++rng_pptr)
-        rng_pool[rng_pptr] = 0;
-      rng_pptr = 0;
-      //rng_pool = null;
-    }
-    // TODO: allow reseeding after first request
-    return rng_state.next();
-  }
+function rng_get_bytes(ba) {
+  var i;
+  for(i = 0; i < ba.length; ++i) ba[i] = rng_get_byte();
+}
 
-  function rng_get_bytes(ba) {
-    var i;
-    for(i = 0; i < ba.length; ++i) ba[i] = rng_get_byte();
-  }
+function SecureRandom() {}
 
-  function SecureRandom() {}
+SecureRandom.prototype.nextBytes = rng_get_bytes;
 
-  SecureRandom.prototype.nextBytes = rng_get_bytes;
-
-  return SecureRandom;
-}, {requires: [
-    './prng4'
-]});
+module.exports = SecureRandom;
